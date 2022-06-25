b25961b9b319b947f6980824c5b83e3906dcc021
canjain404 <99581234+canjain404@users.noreply.github.com>
2022-06-25 13:20:23 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://netcut.cn/1frgm8crb");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
